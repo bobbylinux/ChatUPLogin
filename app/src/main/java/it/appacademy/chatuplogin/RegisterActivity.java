4bf59220c6cb442fd432c32c33604bb23b5e187b
EptaLab <info@eptalab.com>
2018-06-08 15:47:42 +0200

    aggiunta validazione e layout per registrazione

--- app/src/main/java/it/appacademy/chatuplogin/RegisterActivity.java
+++ app/src/main/java/it/appacademy/chatuplogin/RegisterActivity.java
@@ -5,13 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText etConfermaPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        this.etConfermaPassword = findViewById(R.id.etRegPassConf);
     }
 
     public void btnRegistratiClick(View view) {
@@ -24,5 +29,21 @@ public class RegisterActivity extends AppCompatActivity {
 
         Intent intent2 = new Intent(this, LoginActivity.class);
         startActivity(intent2);
+    }
+
+    private boolean nomeValido(String nome) {
+        return nome.length() > 8;
+    }
+
+    private boolean emailValida(String email) {
+        return email.contains("@");
+    }
+
+    private boolean passwordValida(String password) {
+
+        String confermaPassword = this.etConfermaPassword.getText().toString();
+
+        return password.length() > 8 && password.equals(confermaPassword);
+
     }
 }
